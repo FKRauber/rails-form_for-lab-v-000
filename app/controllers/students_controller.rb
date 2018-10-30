200c6b8f0a92d00855fb2790580c21ede1afbaca
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-10-30 01:05:41 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -21,7 +21,7 @@ class StudentsController < ActionController::Base
   end
   def update
     @student = Student.find(params[:id])
-    @student.update(params.require(:student))
+    @student.update(params.require(:student).permit(:first_name, :last_name))
     redirect_to  student_path(@student)
   end
 end
